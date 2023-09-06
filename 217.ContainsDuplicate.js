be1dce1112384cf9e04e89bb078bf2891263a625
JIHYE CHOI <jihyechoi@JIHYEs-MacBook-Pro.local>
2023-09-05 22:56:51 -0700

    set

--- 217.ContainsDuplicate.js
+++ 217.ContainsDuplicate.js
@@ -1,15 +1,29 @@
-const containsDuplicate = function (nums) {
-  for (let i = 0; i < nums.length -1 ; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
+// const containsDuplicate = function (nums) {
+//   for (let i = 0; i < nums.length -1 ; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] === nums[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// };
 
+//Brute force
 //Time: O(n**2)
 //Space: O(1)
+//------------------------------------------------------------------
+//use sort() Time: O(nlogn) Space: O(1)
 
-console.log(containsDuplicate([1, 2, 3, 22]));
+//------------------------------------------------------------------
+//Hash set, use extra memory,  Time: O(n) Space: O(n)
+
+const containsDuplicate = (nums) => {
+  const arr = new Set(nums);
+  if (arr.size === nums.length) {
+    return false;
+  }
+  return true;
+};
+
+console.log(containsDuplicate([1, 2, 3, 3]));
